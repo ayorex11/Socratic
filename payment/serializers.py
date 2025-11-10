2847199d04bfe8239d3ce6cdccac0fcd0aa5bf11
unknown <adewalemaxwell11@gmail.com>
2025-11-10 22:24:23 +0000

    additional validation

--- payment/serializers.py
+++ payment/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 def is_positive(value):
     if value <= 0:
         raise serializers.ValidationError("Amount must be positive.")
+    elif value < 7500:
+        raise serializers.ValidationError("Minimum deposit amount is 7500.")
     return value
 
 class DepositSerializer(serializers.Serializer):
